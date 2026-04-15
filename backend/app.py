bbf7c04f70a295d09cca48315c0ee7bc61c498ec
Preeti Hegde <57839500+preetihegde@users.noreply.github.com>
2026-04-15 20:21:28 +0200

    Fix import path for router in app.py

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from backend.api.api_impl import router
+from api.api_impl import router
 import logging
 
 logger = logging.getLogger(__name__)
